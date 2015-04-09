328b5c69d0926f1047734bde66355259f5f09e7c
Daniel Dörrhöfer <dando@gmx.de>
2015-04-09 17:12:36 +0200

    Added IMAGE_PORT and IMAGE_HEADER. PORT moves to config file soon.

--- globaldefs.h
+++ globaldefs.h
@@ -5,7 +5,9 @@
 #define SIRIUS_URL      "localhost\0"
 #define QUESTION_PORT   ":8080\0"
 #define SPEECH_PORT     ":8081\0"
+#define IMAGE_PORT      ":8082\0"
 #define WAV_HEADER      "Content-Type: audio/vnd.wave; rate=16000"
+#define IMG_HEADER      "Content-Type: image/jpeg"
 #define QUERY           "?query=\0"
 #define VERSION         "0.0.2"
 
