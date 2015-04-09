cae86f2800e296f7996873a078a8b880c4451763
Daniel Dörrhöfer <dando@gmx.de>
2015-04-09 10:36:23 +0200

    Two type of ports QUESTION and SPEECHRECOG.

--- globaldefs.h
+++ globaldefs.h
@@ -1,9 +1,13 @@
 #ifndef GLOBALDEFS_H
 #define GLOBALDEFS_H
 
-#define MAX_DATA_SIZE (2048) 
-#define SIRIUS_URL "http://localhost:8080\0"
-#define VERSION "0.0.1"
+#define MAX_DATA_SIZE   (2048)
+#define SIRIUS_URL      "localhost\0"
+#define QUESTION_PORT   ":8080\0"
+#define SPEECH_PORT     ":8081\0"
+#define WAV_HEADER      "Content-Type: audio/vnd.wave; rate=16000"
+#define QUERY           "?query=\0"
+#define VERSION         "0.0.2"
 
 struct MemoryStruct {
   char *memory;
