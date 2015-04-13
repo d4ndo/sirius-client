a962a193e832f02881dc1f1063f217b9cf528369
Daniel Dörrhöfer <dando@gmx.de>
2015-04-13 08:26:05 +0200

    The sirius host(python) does not support multipart/formdata So it had to be changed to a simple HTTP POST.

--- speech_recognition.c
+++ speech_recognition.c
@@ -2,42 +2,54 @@
 #include <malloc.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <sys/stat.h>
 #include "speech_recognition.h"
 #include "globaldefs.h"
+#include "file2memory.h"
 
+/*
+ * This function is similar to this curl call:
+ *  curl -H "Content-Type: audio/vnd.wave; rate=16000"
+ * --data-binary @what.is.the.capital.of.italy.wav localhost:8081
+ * NO multipart/formdata  HTTP  POST
+ */
 int speech_recog(char *url, unsigned char *wavfile, char **answer) {
 
     CURL *curl_handle;
     CURLcode res;
 
-    char *modified_url = NULL;
-    struct MemoryStruct chunk;
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl_handle = curl_easy_init();
+    curl_easy_setopt(curl_handle, CURLOPT_HEADER, 1L);
+    curl_easy_setopt(curl_handle, CURLOPT_POST, 1L);
+    if (DEBUG) curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+    curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+    curl_easy_setopt(curl_handle, CURLOPT_PROTOCOLS,
+                           CURLPROTO_HTTP | CURLPROTO_HTTPS);
 
+    FILE *fd = NULL;
+    if (!(fd = fopen(wavfile, "rb"))) return -1;
+    char *binaryptr = NULL;
+    size_t binarySize = 0;
+
+    if (0 > file2memory(&binaryptr, &binarySize, fd)) return -1;
+    curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDS, binaryptr);
+    curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDSIZE, (curl_off_t)binarySize);
+
+    struct MemoryStruct chunk;
     chunk.memory = malloc(1);
     chunk.size = 0;
 
-    struct curl_httppost *formpost=NULL;
-    struct curl_httppost *lastptr=NULL;
-    struct curl_slist *headerlist=NULL;
-    static const char buf[] = WAV_HEADER;
-
-    curl_formadd(&formpost,
-                 &lastptr,
-                 CURLFORM_COPYNAME, "sendfile",
-                 CURLFORM_FILE, wavfile,
-                 CURLFORM_END);
-
-    curl_global_init(CURL_GLOBAL_ALL);
-    curl_handle = curl_easy_init();
-
-    headerlist = curl_slist_append(headerlist, buf);
-    curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, headerlist);
-    curl_easy_setopt(curl_handle, CURLOPT_HTTPPOST, formpost);
-    curl_easy_setopt(curl_handle, CURLOPT_URL, url);
-
     curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
     curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "sirius-client");
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, WAV_HEADER);
+
+    curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, headers);
+
     res = curl_easy_perform(curl_handle);
 
     if(res != CURLE_OK) {
@@ -46,8 +58,20 @@ int speech_recog(char *url, unsigned char *wavfile, char **answer) {
         return -1;
     }
 
+    /* verbose output */
+    if (verbose) {
+    double speed_upload, total_time;
+
+    curl_easy_getinfo(curl_handle, CURLINFO_SPEED_UPLOAD, &speed_upload);
+    curl_easy_getinfo(curl_handle, CURLINFO_TOTAL_TIME, &total_time);
+    fprintf(stderr, "Speed: %.3f bytes/sec during %.3f seconds\n",
+                    speed_upload, total_time);
+    }
+
     curl_easy_cleanup(curl_handle);
     curl_global_cleanup();
+    curl_slist_free_all(headers);
+    free(binaryptr);
 
     *answer = chunk.memory;
     return chunk.size;
